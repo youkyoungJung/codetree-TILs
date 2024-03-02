8f37ad95c6818177f8033e0b29dd78516534a9e1
youkyoungJung <76727102+youkyoungJung@users.noreply.github.com>
2024-03-02 23:52:19 +0900

    Title: 빙하, Time: 1533ms, Memory: 52MB, Status: Passed - CodeTree

--- 240302/빙하/glacier.java
+++ 240302/빙하/glacier.java
@@ -1,94 +1,152 @@
-import java.io.*;
-import java.util.*;
+import java.util.Scanner;
+import java.util.Queue;
+import java.util.LinkedList;
 
-class Point {
-    int x;
-    int y;
-
-    Point(int x ,int y) {
-        this.x = x;
-        this.y = y;
-    }
+class Pair {
+    int x, y;
+    public Pair(int x, int y) { 
+        this.x = x; 
+        this.y = y; 
+    } 
 }
 
 public class Main {
-    public static final int MAX_SIZE = 200;
+    public static final int MAX_M = 200;
+    public static final int MAX_N = 200;
+    public static final int DIR_NUM = 4;
+
+    public static final int WATER = 0;
+    public static final int GLACIER = 1;
+    
+    // 전역 변수 선언:
     public static int n, m;
-    public static int[][] grid = new int[MAX_SIZE][MAX_SIZE];
-
-    public static int lastCnt = 0;
-
-    public static boolean[][] isVisited;
-
-    public static Queue<Point> q = new ArrayDeque<>();
-
-    public static int[][] dist = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-
-    public static boolean checked(int x, int y) {
-        return  0 <= x && x < n && 0 <= y && y < m && !isVisited[x][y];
+    
+    public static int[][] a = new int[MAX_N][MAX_M];
+    
+    // bfs에 필요한 변수들 입니다.
+    public static Queue<Pair> q = new LinkedList<>();
+    public static boolean[][] visited = new boolean[MAX_N][MAX_N];
+    public static int cnt;
+    
+    public static int[] dx = new int[]{1, -1, 0, 0};
+    public static int[] dy = new int[]{0, 0, 1, -1};
+    
+    // 소요 시간과 가장 마지막으로 녹은 빙하의 수를 저장합니다.
+    public static int elapsedTime, lastMeltCnt;
+    
+    // 범위가 격자 안에 들어가는지 확인합니다.
+    public static boolean inRange(int x, int y) {
+        return 0 <= x && x < n && 0 <= y && y < m;
     }
-
-
-    public static boolean hasIce() {
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (grid[i][j] == 1)
-                    return true;
-            }
-        }
-        return false;
+    
+    // 범위를 벗어나지 않으면서 물이여야 하고 방문한적이 
+    // 없어야 갈 수 있습니다.
+    public static boolean canGo(int x, int y) {
+        return inRange(x, y) && a[x][y] == WATER && !visited[x][y];
     }
-
-    public static void BFS(int x, int y) {
-
-        isVisited[x][y] = true;
-        q.offer(new Point(x, y));
-
-        int cnt = 0;
-        while (!q.isEmpty()) {
-            Point now = q.poll();
-
-            for (int i = 0; i < 4; i++) {
-                int nx = now.x + dist[i][0];
-                int ny = now.y + dist[i][1];
-                if (checked(nx, ny)) {
-                    isVisited[nx][ny] = true;
-                    if (grid[nx][ny] == 0)
-                        q.offer(new Point(nx, ny));
-                    else {
-                        cnt ++;
-                        grid[nx][ny] = 0;
-                    }
+    
+    // visited 배열을 초기화합니다.
+    public static void initialize() {
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < m; j++)
+                visited[i][j] = false;
+    }
+    
+    // 빙하에 둘러쌓여 있지 않은 물들을 전부 구해주는 BFS입니다.
+    // 문제에서 가장자리는 전부 물로 주어진다 했기 때문에
+    // 항상 (0, 0)에서 시작하여 탐색을 진행하면
+    // 빙하에 둘러쌓여 있지 않은 물들은 전부 visited 처리가 됩니다.
+    public static void BFS() {
+        // BFS 함수가 여러 번 호출되므로
+        // 사용하기 전에 visited 배열을 초기화 해줍니다.
+        initialize();
+    
+        // 항상 (0, 0)에서 시작합니다.
+        q.add(new Pair(0, 0));
+        visited[0][0] = true;
+    
+        while(!q.isEmpty()) {
+            // queue에서 가장 먼저 들어온 원소를 뺍니다.
+            Pair currPos = q.poll();
+            int x = currPos.x, y = currPos.y;
+    
+            // queue에서 뺀 원소의 위치를 기준으로 4 방향을 확인합니다.
+            for(int dir = 0; dir < DIR_NUM; dir++) {
+                int nx = x + dx[dir], ny = y + dy[dir];
+    
+                // 더 갈 수 있는 곳이라면 Queue에 추가합니다.
+                if(canGo(nx, ny)) {
+                    q.add(new Pair(nx, ny));
+                    visited[nx][ny] = true;
                 }
             }
         }
-        lastCnt = cnt;
+    }
+    
+    // 현재 위치를 기준으로 인접한 영역에
+    // 빙하에 둘러쌓여 있지 않은 물이 있는지를 판단합니다.
+    public static boolean outsideWaterExistInNeighbor(int x, int y) {
+        for(int dir = 0; dir < DIR_NUM; dir++) {
+            int nx = x + dx[dir], ny = y + dy[dir];
+            if(inRange(nx, ny) && visited[nx][ny])
+                return true;
+        }
+    
+        return false;
+    }
+    
+    // 인접한 영역에 빙하에 둘러쌓여 있지 않은 물이 있는 빙하를 찾아
+    // 녹여줍니다.
+    public static void melt() {
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < m; j++)
+                if(a[i][j] == GLACIER && 
+                   outsideWaterExistInNeighbor(i, j)) {
+                    a[i][j] = WATER;
+                    lastMeltCnt++;
+                }
+    }
+    
+    // 빙하를 한 번 녹입니다.
+    public static void simulate() {
+        elapsedTime++;
+        lastMeltCnt = 0;
+    
+        // 빙하에 둘러쌓여 있지 않은 물의 위치를 전부 
+        // visited로 체크합니다.
+        BFS();
+    
+        // 인접한 영역에 빙하에 둘러쌓여 있지 않은 물이 있는 빙하를 찾아
+        // 녹여줍니다.
+        melt();
+    }
+    
+    // 빙하가 아직 남아있는지 확인합니다.
+    public static boolean glacierExist() {
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < m; j++)
+                if(a[i][j] == GLACIER)
+                    return true;
+    
+        return false;
     }
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // 입력:
+        n = sc.nextInt();
+        m = sc.nextInt();
 
-        grid = new int[n][m];
-        for(int i = 0; i < n; i++){
-            st = new StringTokenizer(br.readLine());
-            for(int j = 0; j < m; j++){
-                grid[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < m; j++)
+                a[i][j] = sc.nextInt();
+        
+        do {
+            // 빙하를 한 번 녹입니다. 
+            simulate();
+        } while(glacierExist()); // 빙하가 존재하는 한 계속 반복합니다.
 
-        for (int time = 1; time <= n*m; time++) {
-            isVisited = new boolean[n][m];
-
-            BFS(0, 0);
-
-            if(!hasIce()) {
-                System.out.println(time + " " + lastCnt);
-                break;
-            }
-
-        }
+        // 출력:
+        System.out.print(elapsedTime + " " + lastMeltCnt);
     }
 }
