197639e5ad1f792940af1bc3e7a23b9910cc30a4
youkyoungJung <76727102+youkyoungJung@users.noreply.github.com>
2024-03-04 10:05:35 +0900

    Title: 4가지 연산을 이용하여 1 만들기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240304/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.java
@@ -0,0 +1,71 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static int n;
+    static int[] go = {0, 1, 2, 3};
+    static boolean[] isVisited = new boolean[1_000_001];
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        System.out.println(findOne(n));
+
+    } //end of main
+
+    public static int findOne(int n){
+
+        Queue<int[]> queue = new ArrayDeque<>();
+        queue.offer(new int[]{n, 0});
+        isVisited[n] = true;
+
+        while(!queue.isEmpty()){
+            int[] current = queue.poll();
+            if(current[0] == 1){
+                return current[1]; 
+            }
+
+            for(int i = 0; i < 4; i++){
+                int next = 0;
+                if(i == 0){
+                    next = go0(current[0]);
+                }else if (i == 1){
+                    next = go1((current[0]));
+                }else if (i == 2){
+                    next = go2(current[0]);
+                }else{
+                    next = go3(current[0]);
+                }
+
+                if(!isVisited[next]){
+                    queue.offer(new int[]{next, current[1] + 1});
+                    isVisited[next] = true;
+                }
+            }
+        }
+
+        return -1;
+    } //end of bfs
+
+    public static int go0(int n){
+        return n-1;
+    }
+
+    public static int go1(int n){
+        return n+1;
+    }
+
+    public static int go2(int n){
+        if(n % 2 == 0){
+            return n / 2;
+        }
+        return n;
+    }
+    public static int go3(int n){
+        if(n % 3 == 0){
+            return n / 3;
+        }
+        return n;
+    }
+}//end of class
