b6487e7610d5a4c8072c57f03d9ccea9975b8a0a
youkyoungJung <76727102+youkyoungJung@users.noreply.github.com>
2024-03-06 12:23:28 +0900

    Title: 가장 먼저 나오는 숫자, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240306/가장 먼저 나오는 숫자/first-appear-number.java
@@ -0,0 +1,52 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int[] arr;
+    static int n;
+
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        StringBuilder sb = new StringBuilder();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        arr = new int[n + 1];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 1; i <= n; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < m; i++){
+            int target = Integer.parseInt(st.nextToken());
+            sb.append(lowerBound(target)).append("\n");
+        }
+        System.out.println(sb.toString());
+    }
+
+    public static int lowerBound(int target){
+        int left = 1;
+        int right = n;
+        int minIdx = n + 1;
+
+        while(left <= right){
+            int mid = (left + right) /2;
+            if(arr[mid] >= target){
+                right = mid -1 ;
+                minIdx = Math.min(minIdx, mid);
+            }else{
+                left = mid + 1;
+            }
+
+        }
+
+        if(arr[minIdx] != target){
+            minIdx = -1;
+        }
+        return minIdx;
+    }
+}
