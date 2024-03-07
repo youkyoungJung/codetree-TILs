7103f7ce40a4a2b1cbfbaccc69ea09810a13147c
youkyoungJung <76727102+youkyoungJung@users.noreply.github.com>
2024-03-07 11:18:48 +0900

    Title: 정수 분배하기, Time: 93ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240307/정수 분배하기/distributing-integers.java
@@ -0,0 +1,50 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int[] arr;
+    static int n;
+    static int m;
+
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new int[n];
+        int left = 1;
+        int right = Integer.MAX_VALUE;
+
+        for(int i = 0; i < n; i++){
+            arr[i] = Integer.parseInt(br.readLine());
+            right = Math.min(right, arr[i]);
+        }
+
+        int answer = Integer.MIN_VALUE;
+        while(left <= right){
+            int mid = (left + right) / 2;
+
+            if(isPossible(mid)){ //결정에 대한 문제가 Yes
+                left = mid + 1;
+                answer = Math.max(answer, mid);
+            }else{
+                right = mid - 1;
+            }
+
+        }
+        System.out.println(answer);
+
+    }
+
+    public static boolean isPossible(int target){
+        int cnt = 0;
+
+        for(int i = 0; i < n; i++){
+            cnt += (arr[i]/target);
+        }
+
+        return cnt >= m;
+    }
+}
