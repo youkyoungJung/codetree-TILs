f42bddad23cf561d8724a7fdba292de1030fbdc5
youkyoungJung <76727102+youkyoungJung@users.noreply.github.com>
2024-03-08 10:31:30 +0900

    Title: 삼 오 무, Time: 3037ms, Memory: 8MB, Status: Time Limit Exceed - CodeTree

--- 240308/삼 오 무/three-five-moo.java
+++ 240308/삼 오 무/three-five-moo.java
@@ -2,33 +2,32 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+    static int target;
+
     public static void main(String[] args) throws IOException {
         // 여기에 코드를 작성해주세요.
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int target = Integer.parseInt(br.readLine());
+        target = Integer.parseInt(br.readLine());
 
-        int left = 1;
+        int left = 0;
         int right = Integer.MAX_VALUE;
-        int maxIdx = Integer.MIN_VALUE;
 
-        while(left <= right){
+        while(left+1 < right){
             int mid = (left + right) / 2;
 
-            int counting = countNum(mid);
-            if(counting >= target){
-                maxIdx = Math.max(maxIdx, right);
-                right = mid - 1;
+            if(checked(mid)){
+                right = mid;
             }else{
-                left = mid + 1;
+                left = mid;
             }
-
         }
 
-        System.out.println(left);
+        System.out.println(right);
     }
 
-    public static int countNum(int target){
-        return target - (target / 3 + target / 5 - target / 15) ;
+    public static boolean checked(int mid){
+        int res = mid - (mid / 3 + mid / 5 - mid / 15);
+        return res >= target;
     }
 }
 //968460, 1815862
