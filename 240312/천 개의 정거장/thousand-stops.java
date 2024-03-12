c50f64652fc623539de9b79e359d00ade778370d
youkyoungJung <76727102+youkyoungJung@users.noreply.github.com>
2024-03-12 15:50:13 +0900

    Title: 천 개의 정거장, Time: 107ms, Memory: 13MB, Status: Wrong Answer - CodeTree

--- 240312/천 개의 정거장/thousand-stops.java
+++ 240312/천 개의 정거장/thousand-stops.java
@@ -1,130 +1,75 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.sql.Time;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-	static class TimeAndCost implements Comparable<TimeAndCost> {
-		int time;
-		int root;
-		long cost;
-		int nodeNum;
 
-		public TimeAndCost(int time, int root, long cost, int nodeNum) {
-			this.time = time;
-			this.root = root;
-			this.cost = cost;
-			this.nodeNum = nodeNum;
-		}
+    static int[] dist;
+    static int[][] graph;
+    static int INF = (int)1e9;
+    static boolean[] isVisited;
+    static int[] path;
 
-		@Override
-		public int compareTo(TimeAndCost o) {
-			if(this.cost==o.cost){
-				return this.time-o.time;
-			}
-			return Long.compare(this.cost,o.cost);
-		}
-	}
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-	static class Node {
-		long curCost;
-		int curRoot;
-		int nodeNum;
-		Node next;
+        int a = Integer.parseInt(st.nextToken()); //지점 a
+        int b = Integer.parseInt(st.nextToken()); //지점 b
+        int n = Integer.parseInt(st.nextToken()); //버스의 수
 
-		public Node(int curRoot, long curCost, int nodeNum, Node next) {
-			this.curRoot = curRoot;
-			this.curCost = curCost;
-			this.nodeNum = nodeNum;
-			this.next = next;
-		}
+        graph = new int[1001][1001];
+        path = new int[10001];
+        isVisited = new boolean[1001];
+        dist = new int[1001];
+        Arrays.fill(dist, INF);
 
-	}
+        for(int bus = 1; bus <= n; bus++){
+            st = new StringTokenizer(br.readLine());
+            int fee = Integer.parseInt(st.nextToken());
+            int station = Integer.parseInt(st.nextToken());
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+            st = new StringTokenizer(br.readLine());
+            for(int i = 0; i < station; i++){
+                int desti = Integer.parseInt(st.nextToken());
+                graph[desti][station] = fee;
+            }
+        } // 버스 경로 저장
 
-		int A = Integer.parseInt(st.nextToken());
-		int B = Integer.parseInt(st.nextToken());
-		int N = Integer.parseInt(st.nextToken());//노선의 개수
-		Node[] nodes = new Node[1001];
-		for (int i = 1; i <= 1000; i++) {
-			nodes[i] = new Node(0, 0, 0, null);
-		}
-		long[][] minCost = new long[1001][1001];
-		for(int i=1;i<=1000;i++) {
-			Arrays.fill(minCost[i], 200_000_000_000_000L);
-		}
-		for (int i = 1; i <= N; i++) {
-			st = new StringTokenizer(br.readLine());
-			long cost = Long.parseLong(st.nextToken());
-			int stationCount = Integer.parseInt(st.nextToken());
+        dijkstra(b);
+        System.out.println(dist[a]+ " " + graph[path[a]][a]);
+        
 
-			st = new StringTokenizer(br.readLine());
+    }
 
-			int from = Integer.parseInt(st.nextToken());
-			for (int j = 0; j < stationCount - 1; j++) {
-				int to = Integer.parseInt(st.nextToken());
+    public static void dijkstra(int start){
+        dist[start] = 0;
 
-				nodes[from].next = new Node(i, cost, to, nodes[from].next);
-				//minCost[from][to]=Math.min(minCost[from][to],cost);
+        for(int i = 1; i <= 1000; i++){
+            int minIdx = -1;
 
-				from = to;
-			}
-		}//입력 종료
+            for(int j = 1; j <= 1000; j++){
+                if(isVisited[j]) continue;
 
-		PriorityQueue<TimeAndCost> q = new PriorityQueue<>();
-		//time root cost num 순
-		for (Node cur = nodes[A].next; cur != null; cur = cur.next) {
-			q.add(new TimeAndCost(1, cur.curRoot, cur.curCost, cur.nodeNum));
-		}
+                if(minIdx == -1 || dist[minIdx] > dist[j]){
+                    minIdx = j;
+                }
+            }
 
-		long resultCost = 200_000_000_000_000L;
-		long resultTime = 200_000_000_000_000L;
+            isVisited[minIdx] = true; // 정점 방문 표시
 
-		while (!q.isEmpty()) {
-			TimeAndCost cur = q.poll();
-			// System.out.println(cur.root+" "+cur.nodeNum+" "+cur.cost);
-			int cNum = cur.nodeNum;
-			if (cNum == B) {
-				// if(resultCost>=cur.cost){
-				// 	resultTime = Math.min(resultTime,cur.time);
-				// 	resultCost = cur.cost;
-				// }
-				resultTime = cur.time;
-				resultCost = cur.cost;
-				break;
-			}
+            for(int j = 1; j <= 1000; j++){
+                if(graph[minIdx][j] == 0) continue;
 
-			for (Node tmp = nodes[cNum].next; tmp != null; tmp = tmp.next) {
-				long cmpCost = 0;
-				int cmpRoot = 0;
+                //통행료 검사
+                int tranfee = graph[minIdx][j];
+                if(dist[minIdx] != tranfee){
+                    dist[j] = Math.min(dist[j], dist[minIdx] + graph[minIdx][j]);
+                    path[j] = minIdx;
+                }
+            }
 
-				if (tmp.curRoot == cur.root) {//같은 노선이면 돈을 더 내지 않음
-					cmpCost = cur.cost;
-					cmpRoot = cur.root;
-				} else {//다른 노선일 경우
-					cmpCost = cur.cost + tmp.curCost;
-					cmpRoot = tmp.curRoot;
-				}
-				// time root cost num 순
-				if (minCost[cur.nodeNum][tmp.nodeNum] < cmpCost) {//이미 더 싸게 갈수있는법이 있을 때
-					continue;
-				} else if (minCost[cur.nodeNum][tmp.nodeNum] >= cmpCost) {
-					minCost[cur.nodeNum][tmp.nodeNum] = cmpCost;
-					q.add(new TimeAndCost(cur.time + 1, cmpRoot, cmpCost, tmp.nodeNum));
-				}
-				// q.add(new TimeAndCost(cur.time + 1,cmpRoot, cmpCost, tmp.nodeNum));
-			}
-		}
-		if (resultCost == 200_000_000_000_000L && resultTime == 200_000_000_000_000L)
-			System.out.println("-1 -1");
-		else
-			System.out.println(resultCost + " " + resultTime);
-	}
+        }
+    }
+
 }
