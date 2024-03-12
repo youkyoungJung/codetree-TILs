242e0682cea47bf3ba176a9aab1a662df96bf1f9
youkyoungJung <76727102+youkyoungJung@users.noreply.github.com>
2024-03-12 13:05:06 +0900

    Title: 최단 거리 3, Time: 297ms, Memory: 22MB, Status: Wrong Answer - CodeTree

--- 240312/최단 거리 3/shortest-distance-3.java
+++ 240312/최단 거리 3/shortest-distance-3.java
@@ -3,39 +3,26 @@ import java.io.*;
 
 public class Main {
 
-    static class Edge implements Comparable<Edge>{
-
-        int num;
-        int val;
-        
-        public Edge(int num, int val){
-            this.num = num;
-            this.val = val;
-        }
-
-        public int compareTo(Edge o){
-            return Integer.compare(this.val, o.val);
-        }
-    }
-
-    static ArrayList<ArrayList<Edge>> graph;
-    static int[] dist;
+    static int[] nums;
+    static int n;
     static int INF = (int)1e9;
 
+    static int[][] graph;
+    static boolean[] isVisited;
 
-    public static void main(String[] args) throws IOException {
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args)throws IOException {
         // 여기에 코드를 작성해주세요.
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
-        graph = new ArrayList<>(n+1);
-        for(int i = 0; i <= n; i++){
-            graph.add(new ArrayList<>());
-        }
-        dist = new int[n+1];
-        Arrays.fill(dist, INF);
+        nums = new int[n+1];
+        isVisited = new boolean[n+1];
+        graph = new int[n+1][n+1];
+        Arrays.fill(nums, INF);
 
         for(int i = 0; i < m; i++){
             st = new StringTokenizer(br.readLine());
@@ -43,37 +30,41 @@ public class Main {
             int e = Integer.parseInt(st.nextToken());
             int v = Integer.parseInt(st.nextToken());
 
-            graph.get(s).add(new Edge(e, v));
-            graph.get(e).add(new Edge(s, v));
+            graph[s][e] = v;
+            graph[e][s] = v;
         }
 
         st = new StringTokenizer(br.readLine());
         int a = Integer.parseInt(st.nextToken());
         int b = Integer.parseInt(st.nextToken());
 
-        dijkstra(b);
-        System.out.println(dist[a]);
+        dijkstra(a);
+        System.out.println(nums[b]);
+
     }
 
     public static void dijkstra(int start){
+        nums[start] = 0;
 
-        dist[start] = 0;
-        PriorityQueue<Edge> pq = new PriorityQueue<>();
-        pq.offer(new Edge(start, 0));
+        for(int i = 1; i <= n; i++){
+            int minIdx = -1;
 
-        while(!pq.isEmpty()){
-            Edge current = pq.poll();
+            for(int j = 1; j <= n; j++){
+                if(isVisited[j]) continue;
 
-            for(int i = 0; i < graph.get(current.num).size(); i++){
-                Edge next = graph.get(current.num).get(i);
-
-                int target = dist[current.num] + next.val;
-                if(dist[next.num] > target){
-                    dist[next.num] = target;
-                    pq.offer(new Edge(next.num, target));
+                if(minIdx == -1 || nums[minIdx] > nums[j]){
+                    minIdx = j;
                 }
             }
+
+            isVisited[minIdx] = true; // 정점 방문 표시
+
+            for(int j = 1; j <= n; j++){
+                if(graph[minIdx][j] == 0) continue;
+
+                nums[j] = Math.min(nums[j], nums[minIdx] + graph[minIdx][j]);
+            }
+
         }
-    
     }
 }
