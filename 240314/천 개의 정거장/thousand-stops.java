4bde9272cc0c4310f64f1051dafce9fefcc14f9a
youkyoungJung <76727102+youkyoungJung@users.noreply.github.com>
2024-03-14 09:44:12 +0900

    Title: 천 개의 정거장, Time: 611ms, Memory: 23MB, Status: Passed - CodeTree

--- 240314/천 개의 정거장/thousand-stops.java
+++ 240314/천 개의 정거장/thousand-stops.java
@@ -1,129 +1,174 @@
 import java.util.*;
 import java.io.*;
 
-
-//버스 노선의 수 N, 노선을 이루는 점의 개수 범위 R
-//처음 그래프를 만드는데 걸리는 시간 O(NR^2)
-//다익스트라(인접 행렬) - O(M^2)
 public class Main {
 
-    static class Pair {
-        long cost;
-        long time;
+	static int A, B, N;
+	static boolean[] visited;
+	static Node[] dist;
 
-        public Pair(long cost, long time){
-            this.cost = cost;
-            this.time = time;
-        }
-        // 비용이 동일할 시 시간이 가장 짧은 경우를 갱신하도록 설계
-        public boolean isGreaterThan(Pair p){
-            return this.cost > p.cost || (this.cost == p.cost && this.time > p.time);
-        }
-    }
+	static ArrayList<ArrayList<Edge>> graph;
 
-    public static final int INT_MAX = Integer.MAX_VALUE;
-    public static final long INF = (long)1e17 + 1;
-    public static final int MAX_M = 1000;
+	static class Edge implements Comparable<Edge> {
+		int to, line, time;
+		long cost;
 
-    public static int a, b, n, m = 1000;
-    public static Pair[][] graph = new Pair[1001][1001];
-    public static boolean[] isVisited = new boolean[10001];
+		public Edge(int to, int line, long cost, int time) {
+			this.to = to;
+			this.line = line;
+			this.cost = cost;
+			this.time = time;
+		}
 
-    public static Pair[] dist = new Pair[1001];
+		@Override
+		public int compareTo(Edge e) {
+			if (this.cost == e.cost) {
+				return Long.compare(this.time, e.time);
+			}
+			return Long.compare(this.cost, e.cost);
+		}
 
-    public static void init(){
+		@Override
+		public String toString() {
+			return "Edge{" +
+				"to=" + to +
+				", line=" + line +
+				", time=" + time +
+				", cost=" + cost +
+				'}';
+		}
+	}
 
-        // 초기 graph 값에 전부 큰 값 기록
-        for(int i = 1; i <= m; i++){
-            // 초기 dist
-            dist[i] = new Pair(INF, 0);
+	static class Node implements Comparable<Node> {
+		int time;
+		long cost;
 
-            for(int j = 1; j <= m; j++){
-                graph[i][j] = new Pair(INF, 0);
-            }
-            // 자기 자신은 비용과 시간이 전혀 소요되지 않음.
-            graph[i][i] = new Pair(0, 0);
-        }
+		public Node(long cost, int time) {
+			this.cost = cost;
+			this.time = time;
+		}
 
-    }
-    public static void main(String[] args) throws IOException{
-        // 주어진 버스 노선으로부터 각 지점 쌍에 대해 최소 비용과 최소시간을 담고 있는 그래프 완성
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int a = Integer.parseInt(st.nextToken()); //지점 a
-        int b = Integer.parseInt(st.nextToken()); //지점 b
-        int n = Integer.parseInt(st.nextToken()); //버스의 수
+		@Override
+		public int compareTo(Node n) {
+			if (this.cost == n.cost) {
+				return Long.compare(this.time, n.time);
+			}
+			return Long.compare(this.cost, n.cost);
+		}
 
-        init();
+		@Override
+		public String toString() {
+			return "Node{" +
+				"time=" + time +
+				", cost=" + cost +
+				'}';
+		}
+	}
 
-        //인접 행렬 이용 O(M^2)
-        // 비용 정보, 걸린 시간 정보 같이 관리
-        for(int bus = 1; bus <= n; bus++){
-            st = new StringTokenizer(br.readLine());
-            int fee = Integer.parseInt(st.nextToken());
-            int stopNum = Integer.parseInt(st.nextToken());
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
-            ArrayList<Integer> stations = new ArrayList<>();
-            st = new StringTokenizer(br.readLine());
-            for(int i = 0; i < stopNum; i++){
-                int x = Integer.parseInt(st.nextToken());
-                stations.add(x);
-            }
-            // 최소 비용이 동일한 경우 - 가능한 시간 중 최솟값 갱신
-            for(int i = 0; i < stopNum; i++){
-                for(int j = i + 1; j < stopNum; j++){
-                    Pair newPair = new Pair(fee, j-i);
-                    if(graph[stations.get(i)][stations.get(j)].isGreaterThan(newPair)){
-                        graph[stations.get(i)][stations.get(j)] = newPair;
-                    }
-                }
-            }
+		A = Integer.parseInt(st.nextToken());
+		B = Integer.parseInt(st.nextToken());
+		N = Integer.parseInt(st.nextToken());
 
-        } // 버스 경로 저장
+		visited = new boolean[1001];
+		graph = new ArrayList<>(1001);
+		for (int i = 0; i < 1001; i++) {
+			graph.add(new ArrayList<>());
+		}
+		dist = new Node[1001];
+		for (int i = 0; i < 1001; i++) {
+			dist[i] = new Node(Long.MAX_VALUE, Integer.MAX_VALUE);
+		}
 
-        dijkstra(a);
+		for (int i = 1; i <= N; i++) {
+			st = new StringTokenizer(br.readLine());
 
-        // 만약 도달이 불가능 하다면 -1, -1
-        if(dist[b].cost == INF) dist[b] = new Pair(-1, -1);
+			long cost = Long.parseLong(st.nextToken());
+			int n = Integer.parseInt(st.nextToken());
 
-        long minCost = dist[b].cost;
-        long minTime = dist[b].time;
+			int[] temp = new int[n];
+			st = new StringTokenizer(br.readLine());
+			int aIdx = n;
+			for (int j = 0; j < n; j++) {
+				int x = Integer.parseInt(st.nextToken());
+				if (x == A) {
+					aIdx = j;
+				}
+				temp[j] = x;
+			}
+			for (int j = 0; j < n - 1; j++) {
+				Edge e = new Edge(temp[j + 1], i, cost, 1);
+				graph.get(temp[j]).add(e);
+			}
+			for (int j = aIdx + 1; j < n; j++) {
+				if (dist[temp[j]].cost > cost) {
+					dist[temp[j]].cost = cost;
+					dist[temp[j]].time = j - aIdx;
+				} else if (dist[temp[j]].cost == cost) {
+					dist[temp[j]].time = Math.min(dist[temp[j]].time, j - aIdx);
+				}
+			}
+			// for (int k = 0; k < 6; k++) {
+			// 	System.out.println(dist[k]);
+			// }
+			// System.out.println("=======");
+		}
+		// for (int i = 1; i < 1001; i++) {
+		// 	System.out.println(dist[i]);
+		// }
 
-        System.out.println(minCost + " " + minTime);
+		dijkstra(A);
 
-    }
+		if (dist[B].cost == Long.MAX_VALUE) {
+			System.out.println("-1 -1");
+		} else {
+			System.out.println(dist[B].cost + " " + dist[B].time);
+		}
+	}
 
-    public static void dijkstra(int start){
-        dist[start] = new Pair(0, 0);
+	public static void dijkstra(int start) {
+		PriorityQueue<Edge> pq = new PriorityQueue<>();
 
-        for(int i = 1; i <= 1000; i++){
-            int minIdx = -1;
+		for (Edge edge : graph.get(start)) {
+			pq.add(edge);
+		}
 
-            for(int j = 1; j <= 1000; j++){
-                if(isVisited[j]) continue;
+		// pq.add(new Edge(start, -1, 0, 0));
 
-                if(minIdx == -1 || dist[minIdx].isGreaterThan(dist[j])){
-                    minIdx = j;
-                }
-            }
+		dist[start] = new Node(0, 0);
 
-            isVisited[minIdx] = true; // 정점 방문 표시
-
-            long minCost = dist[minIdx].cost;
-            long minTime = dist[minIdx].time;
-
-            // 최솟값에 해당하는 정점에 연결된 간선들을 보며 시작점으로부터의 최단거리 값을 갱신
-            for(int j = 1; j <= 1000; j++){
-                long cost = graph[minIdx][j].cost;
-                long time = graph[minIdx][j].time;
-
-                Pair newPair = new Pair(minCost + cost, minTime + time);
-                if(dist[j].isGreaterThan(newPair)){
-                    dist[j] = newPair;
-                }
-            }
-
-        }
-    }
+		while (!pq.isEmpty()) {
+			Edge poll = pq.poll();
+			// System.out.println("poll = " + poll);
+			for (int i = 0; i < graph.get(poll.to).size(); i++) {
+				Edge cur = graph.get(poll.to).get(i);
+				if (cur.line == poll.line) {
+					if (dist[cur.to].cost > poll.cost) {
+						dist[cur.to].cost = poll.cost;
+						dist[cur.to].time = poll.time + 1;
+					} else if (dist[cur.to].cost == poll.cost) {
+						if (dist[cur.to].time >= poll.time + 1) {
+							dist[cur.to].time = poll.time + 1;
+						}
+					}
+					pq.add(new Edge(cur.to, cur.line, poll.cost, poll.time + 1));
+				} else {
+					if (dist[cur.to].cost > poll.cost + cur.cost) {
+						dist[cur.to].cost = poll.cost + cur.cost;
+						dist[cur.to].time = poll.time + 1;
+						pq.add(new Edge(cur.to, cur.line, poll.cost + cur.cost, poll.time + 1));
+					} else if (dist[cur.to].cost == poll.cost + cur.cost) {
+						if (dist[cur.to].time >= poll.time + 1) {
+							dist[cur.to].time = poll.time + 1;
+						pq.add(new Edge(cur.to, cur.line, poll.cost + cur.cost, poll.time + 1));
+						}
+					}
+				}
+			}
+		}
+	}
 
 }
