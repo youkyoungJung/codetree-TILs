3e53bb14fb5fae6044bedc2d3c9531bec50bd185
youkyoungJung <76727102+youkyoungJung@users.noreply.github.com>
2024-03-20 10:45:21 +0900

    Title: 이상한 진수 2, Time: 107ms, Memory: 8MB, Status: Passed - CodeTree

--- 240320/이상한 진수 2/awkward-digits-2.java
+++ 240320/이상한 진수 2/awkward-digits-2.java
@@ -5,26 +5,26 @@ public class Main {
 
     static String a;
     static int max = -1;
-    static int[] arr;
+    static char[] arr;
     static StringBuilder sb = new StringBuilder();
 
 	public static void main(String[] args) throws IOException {
 		// 여기에 코드를 작성해주세요.
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String a = br.readLine();
-        arr = new arr[a.length()];
+        arr = new char[a.length()];
         for(int i = 0; i < a.length(); i++){
             arr[i] = a.charAt(i);
         }
         for(int i = 0; i < a.length(); i++){
-            sb.delete();
-            int[] temp = arr.clone();
+            sb.setLength(0);
+            char[] temp = arr.clone();
             if(temp[i] == '0')
-                temp[i] = '1'
+                temp[i] = '1';
             else
                 temp[i] = '0';
             for(int j = 0; j < a.length(); j++){
-                sb.append(temp[i]);
+                sb.append(temp[j]);
             }
             max = Math.max(Integer.parseInt(sb.toString(), 2), max);
         }
