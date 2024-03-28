24df96746a3c454416934101e6b93620d2b9bfeb
youkyoungJung <76727102+youkyoungJung@users.noreply.github.com>
2024-03-28 09:48:01 +0900

    Title: 행렬로 주어진 간선, Time: 103ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240328/행렬로 주어진 간선/edge-given-by-matrix.java
+++ 240328/행렬로 주어진 간선/edge-given-by-matrix.java
@@ -4,12 +4,13 @@ import java.io.*;
 public class Main {
     static int n;
     static int[][] dist;
+    static int[][] isVisited;
     public static void main(String[] args) throws IOException {
         // 여기에 코드를 작성해주세요.
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
         dist = new int[n+1][n+1];
-
+        isVisited = new int[n+1][n+1];
 
         for(int i = 1; i <= n; i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
@@ -21,7 +22,7 @@ public class Main {
         for(int k = 1; k <= n; k++){
             for(int i = 1; i <= n; i++){
                 for(int j = 1; j <= n; j++){
-                    if(dist[i][k] == 1 && dist[k][j] == 1){
+                    if(dist[i][k] == 1 || dist[k][j] == 1){
                         dist[i][j] = 1;
                     }
                 }
