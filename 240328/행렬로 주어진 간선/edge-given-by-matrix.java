53744eff4171bfbfe02019d43ab045ebcea501a2
youkyoungJung <76727102+youkyoungJung@users.noreply.github.com>
2024-03-28 09:51:23 +0900

    Title: 행렬로 주어진 간선, Time: 82ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240328/행렬로 주어진 간선/edge-given-by-matrix.java
+++ 240328/행렬로 주어진 간선/edge-given-by-matrix.java
@@ -9,20 +9,20 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
-        dist = new int[n+1][n+1];
-        isVisited = new int[n+1][n+1];
+        dist = new int[n][n];
+        isVisited = new int[n][n];
 
-        for(int i = 1; i <= n; i++){
+        for(int i = 0; i < n; i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
-            for(int j = 1; j <= n; j++){
+            for(int j = 0; j < n; j++){
                 dist[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        for(int k = 1; k <= n; k++){
-            for(int i = 1; i <= n; i++){
-                for(int j = 1; j <= n; j++){
-                    if(dist[i][k] == 1 || dist[k][j] == 1){
+        for(int k = 0; k < n; k++){
+            for(int i = 0; i < n; i++){
+                for(int j = 0; j < n; j++){
+                    if(dist[i][k] == 1 && dist[k][j] == 1){
                         dist[i][j] = 1;
                     }
                 }
@@ -30,8 +30,8 @@ public class Main {
         }
 
         StringBuilder sb = new StringBuilder();
-        for(int i = 1; i <= n; i++){
-            for(int j = 1; j <= n; j++){
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
                 sb.append(dist[i][j]).append(" ");
             }
             sb.append("\n");
