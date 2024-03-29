808333f08195628f4dec007033bd6228c0774c02
youkyoungJung <76727102+youkyoungJung@users.noreply.github.com>
2024-03-29 16:23:30 +0900

    Title: 최단 거리, Time: 144ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240329/최단 거리/shortest-distance.java
@@ -0,0 +1,60 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int n;
+    static int m;
+    static int[][] dp;
+
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        int[][] graph = new int[n][n];
+        dp = new int[n][n];
+
+        for(int i = 0; i < n; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < n; j++){
+                graph[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        init();
+
+        for(int k = 0; k < n; k++){
+            for(int i = 0; i < n; i++){
+                for(int j = 0; j < n; j++){
+                    if(dp[i][j] > graph[i][k] + graph[k][j]){
+                        dp[i][j] = graph[i][k] + graph[k][j];
+                    }
+                }
+            }
+        }
+
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < m; i++){
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            sb.append(dp[a-1][b-1]).append("\n");
+        }
+        System.out.println(sb.toString());
+
+    }
+
+    public static void init(){
+        
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                dp[i][j] = (int)1e9;
+            }
+            dp[i][i] = 0;
+        }
+    }
+}
