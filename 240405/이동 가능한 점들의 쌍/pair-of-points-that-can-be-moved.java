1ef62dd07c4f64ad26792f91f86a882d83131e48
youkyoungJung <76727102+youkyoungJung@users.noreply.github.com>
2024-04-05 13:55:52 +0900

    Title: 이동 가능한 점들의 쌍, Time: 197ms, Memory: 12MB, Status: Wrong Answer - CodeTree

--- 240405/이동 가능한 점들의 쌍/pair-of-points-that-can-be-moved.java
+++ 240405/이동 가능한 점들의 쌍/pair-of-points-that-can-be-moved.java
@@ -1,71 +1,65 @@
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static final int INT_MAX = Integer.MAX_VALUE;
-    public static final int MAX_N = 200;
-    
-    // 변수 선언
-    public static int n, m, p, q;
-    public static int[][] dist = new int[MAX_N + 1][MAX_N + 1];
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        // 입력
-        n = sc.nextInt();
-        m = sc.nextInt();
-        p = sc.nextInt();
-        q = sc.nextInt();
+    static int INF = (int)1e9;
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int p = Integer.parseInt(st.nextToken());
+        int q = Integer.parseInt(st.nextToken());
 
-        // dist 초기값을 전부 아주 큰 값으로 설정
-        // INT_MAX 그 자체로 설정하면
-        // 후에 덧셈 진행시 overflow가 발생할 수도 있으므로
-        // 적당히 큰 숫자로 적어줘야함에 유의!
-        for(int i = 1; i <= n; i++) {
-            for(int j = 1; j <= n; j++)
-                dist[i][j] = (int)1e9;
-
-            // 자기 자신으로 가는 값은 0으로 설정해줘야 함에 유의합니다.
-            dist[i][i] = 0;
+        int[][] graph = new int[n+1][n+1];
+        //init
+        for(int i = 0; i <= n; i++){
+            for(int j = 0; j <= n; j++){
+                graph[i][j] = INF;
+            }
+            graph[i][i] = 0;
         }
 
-        // 그래프를 인접행렬로 표현
-        while(m-- > 0) {
-            int x = sc.nextInt();
-            int y = sc.nextInt();
-            int z = sc.nextInt();
-            dist[x][y] = z;
+        for(int i = 0; i < m; i++){
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+
+            graph[s][e] = v;
         }
-        
-        for(int k = 1; k <= n; k++) // 확실하게 거쳐갈 정점을 1번부터 N번까지 순서대로 정의합니다.
-            for(int i = 1; i <= n; i++) // 고정된 k에 대해 모든 쌍 (i, j)를 살펴봅니다.
-                for(int j = 1; j <= n; j++)
-                    // i에서 j로 가는 거리가 k를 경유해 가는 것이 더 좋다면
-                    // dist[i][j]값을 갱신해줍니다.
-                    dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
-        
-        long ans = 0;
-        int cnt = 0;
 
-        // q개의 질문에 대해 최단 거리를 답변합니다.
-        for(int i = 1; i <= q; i++) {
-            int x = sc.nextInt();
-            int y = sc.nextInt();
+        //floydWarshall
 
-            int distance = (int)1e9;
-            // 모든 p개의 빨간 점에 대해, 이동 가능한지 확인합니다.
-            for(int j = 1; j <= p; j++) {
-                distance = Math.min(distance, dist[x][j] + dist[j][y]);
+        for(int k = 1; k <= n; k++){
+            for(int i = 1; i <= n; i++){
+                for(int j = 1; j <= n; j++){
+                    graph[i][j] = Math.min(graph[i][j], graph[i][k] + graph[k][j]);
+                }
+            }
+        }
+
+        int isGo = 0;
+        int answer = 0;
+        for(int testCase = 0; testCase < q; testCase++){
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+
+            int dist = (int)1e9;
+            for(int i = 1; i <= p; i++){
+                dist = Math.min(dist, graph[start][i] + graph[i][end]);
             }
 
-            // 만약 이동 불가능하다면, 넘어갑니다.
-            if(distance >= (int)1e9)
-                continue;
-                
-            ans += distance;
-            cnt++;
-        }
+            if(dist == (int)1e9) continue;
 
-        System.out.println(cnt);
-        System.out.print(ans);
+            answer += dist;
+            isGo++;
+
+        }
+        System.out.println(isGo);
+        System.out.println(answer);
     }
 }
