65d063293e366cfc2e3fb940e5b1272a3ed68da3
youkyoungJung <76727102+youkyoungJung@users.noreply.github.com>
2024-04-08 10:25:18 +0900

    Title: 선분 위의 점, Time: 184ms, Memory: 13MB, Status: Wrong Answer - CodeTree

---
+++ 240408/선분 위의 점/point-on-the-line-segment.java
@@ -0,0 +1,61 @@
+import java.io.*;
+import java.lang.reflect.Array;
+import java.util.*;
+
+public class Main {
+
+	static int n, m;
+	static int[] arr;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		arr = new int[n];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+
+		StringBuilder sb = new StringBuilder();
+
+		for (int i = 0; i < m; i++) {
+			st = new StringTokenizer(br.readLine());
+			int start = Integer.parseInt(st.nextToken());
+			int end = Integer.parseInt(st.nextToken());
+			sb.append(upper(end)-lower(start)).append("\n");
+		}
+
+		System.out.println(sb);
+
+	}
+
+	public static int upper(int x) {
+		int left = 0, right = n;
+		while (left < right) {
+			int mid = (left + right) / 2;
+			if (arr[mid] <= x) {
+				left = mid + 1;
+			} else {
+				right = mid;
+			}
+		}
+		return right;
+	}
+
+	public static int lower(int x) {
+		int left = 0, right = n;
+		while (left < right) {
+			int mid = (left + right) / 2;
+			if (arr[mid] < x) {
+				left = mid + 1;
+			} else {
+				right = mid;
+			}
+		}
+		return right;
+	}
+
+}
