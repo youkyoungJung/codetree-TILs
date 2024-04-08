d0ce3b3f36ef035ac78cdc0fcd80837e7d597647
youkyoungJung <76727102+youkyoungJung@users.noreply.github.com>
2024-04-08 12:50:25 +0900

    Title: 정수 분배하기, Time: 97ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240408/정수 분배하기/distributing-integers.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.lang.reflect.Array;
+import java.util.*;
+
+public class Main {
+
+	static int[] arr;
+	static int n, m;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		arr = new int[n];
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		int left = 1, right = 10001;
+		int answer = -1;
+		while(left <= right){
+			int mid = (left + right) /2;
+			if(div(mid) >= m){
+				left = mid +1;
+				answer = Math.max(answer, mid);
+			} else{
+				right = mid - 1;
+			}
+		}
+		System.out.println(answer);
+	}
+
+	public static int div(int x){
+		int cnt = 0;
+		for (int i : arr) {
+			cnt += i/x;
+		}
+		return cnt;
+	}
+
+}
