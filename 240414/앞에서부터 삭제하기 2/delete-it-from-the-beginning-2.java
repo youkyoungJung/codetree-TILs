7c059d618fb830a7499061e96af277b4e5264fc0
youkyoungJung <76727102+youkyoungJung@users.noreply.github.com>
2024-04-14 00:37:22 +0900

    Title: 앞에서부터 삭제하기 2, Time: 267ms, Memory: 21MB, Status: Wrong Answer - CodeTree

--- 240414/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.java
+++ 240414/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.java
@@ -10,18 +10,26 @@ public class Main {
 
         int[] arr = new int[n+1];
         int[] dp = new int[n+1];
+        int[] right = new int[n+1];
         StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i = 1; i <= n; i++){
             arr[i] = Integer.parseInt(st.nextToken());
         }
-        Arrays.sort(arr);
+        // Arrays.sort(arr);
 
         for(int i = 1; i <= n; i++){
             dp[i] = dp[i-1] + arr[i];
         }
+
+        int temp = Integer.MAX_VALUE;
+        for(int i = n; i >= 1; i--){
+            right[i] = Math.min(temp, arr[i]);
+            temp = right[i];
+        }
+
         // System.out.println(Arrays.toString(dp));
         for(int i = 1; i <= n-2; i++){
-            double avg = (double)((dp[n] - dp[i] ) / (n-i));
+            double avg = (double)((dp[n] - dp[i] - right[i+1]) / (n-i-1));
             answer = Math.max(answer, avg);
         }
 
