6970bb4c9cb2f7dcc877dc88cf9cffe251723530
youkyoungJung <76727102+youkyoungJung@users.noreply.github.com>
2024-04-13 14:48:44 +0900

    Title: 자리 차지하기, Time: 206ms, Memory: 17MB, Status: Wrong Answer - CodeTree

---
+++ 240413/자리 차지하기/take-place.java
@@ -0,0 +1,41 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        TreeSet<Integer> set = new TreeSet<>();
+        for(int i = 1; i <= m; i++){
+            set.add(i);
+        }
+
+        int answer = 0;
+
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < n; i++){
+            int num = Integer.parseInt(st.nextToken());
+            if(!set.contains(num)){ //num 이 없어
+                for(int j = num-1; j >= 1; j--){
+                    if(set.contains(j)){
+                        set.remove(j);
+                        answer++;
+                        break;
+                    }else{
+                        return;
+                    }
+                }
+            }else{
+                set.remove(num);
+                answer++;
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
