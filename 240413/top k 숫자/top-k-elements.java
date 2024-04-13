30b764471c59bfd1fd19310730a5d2ab163bf0a6
youkyoungJung <76727102+youkyoungJung@users.noreply.github.com>
2024-04-13 14:36:47 +0900

    Title: top k 숫자, Time: 586ms, Memory: 26MB, Status: Passed - CodeTree

---
+++ 240413/top k 숫자/top-k-elements.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        TreeSet<Integer> set = new TreeSet<>((o1, o2)-> {return Integer.compare(o2, o1);});
+
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < n; i++){
+            int num = Integer.parseInt(st.nextToken());
+            set.add(num);
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < m; i++){
+            sb.append(set.first()).append(" ");
+            set.remove(set.first());
+        }
+
+        System.out.println(sb.toString());
+    }
+}
