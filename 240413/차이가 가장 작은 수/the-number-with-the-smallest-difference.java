9cdbdc33f202a8515ff4c4824ed87ab8921ad219
youkyoungJung <76727102+youkyoungJung@users.noreply.github.com>
2024-04-13 16:00:34 +0900

    Title: 차이가 가장 작은 수, Time: 233ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240413/차이가 가장 작은 수/the-number-with-the-smallest-difference.java
+++ 240413/차이가 가장 작은 수/the-number-with-the-smallest-difference.java
@@ -2,7 +2,7 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int INF = (int)1e9 + 1;
+    static int INF = (int)1e9+1;
     public static void main(String[] args) throws IOException {
         // 여기에 코드를 작성해주세요.
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -10,23 +10,23 @@ public class Main {
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
-        int[] arr = new int[n];
+        TreeSet<Integer> set = new TreeSet<>();
         int answer = INF;
         for(int i = 0; i < n; i++){
-            arr[i] = Integer.parseInt(br.readLine()); 
+            set.add(Integer.parseInt(br.readLine()));
         } //end of for
 
-        Arrays.sort(arr);
+        ArrayList<Integer> arr = new ArrayList<>(set);
 
         int s = 0;
-        int e = n-1;
+        int e = arr.size()-1;
 
         while(s <= e){
-            int target = arr[s];
-            // System.out.println("s: " + target);
-            // System.out.println("e: " + arr[e]);
-            if(m <= Math.abs(arr[e] - target)){
-                answer = Math.min(answer, Math.abs(arr[e] - target));
+            int target = arr.get(s);
+//             System.out.println("s: " + target);
+//             System.out.println("e: " + arr.get(e));
+            if(m <= Math.abs(arr.get(e) - target)){
+                answer = Math.min(answer, Math.abs(arr.get(e) - target));
                 e--;
             }else{
                 s++;
