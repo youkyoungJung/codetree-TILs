37a52bb086b036ce36fc1781c41b3262676f2149
youkyoungJung <76727102+youkyoungJung@users.noreply.github.com>
2024-04-14 14:33:20 +0900

    Title: 요일 맞추기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240414/요일 맞추기/guess-day-of-week.java
+++ 240414/요일 맞추기/guess-day-of-week.java
@@ -17,7 +17,8 @@ public class Main {
         //1    2    3   4   5   6    7    8   9   10   11  12
         int[] monthOfDay = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
-        int diff = ((monthOfDay[m2] + d2) - (monthOfDay[m1] + d1)) + 2;
+        int diff = 0;
+        diff = ((monthOfDay[m2] + d2) - (monthOfDay[m1] + d1)) + 2;
 
         if(diff < 0 ){
             diff += 7;
