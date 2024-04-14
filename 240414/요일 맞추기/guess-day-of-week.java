eda0e8adeccaff14cdfc5095d0a60a3d00589556
youkyoungJung <76727102+youkyoungJung@users.noreply.github.com>
2024-04-14 14:32:46 +0900

    Title: 요일 맞추기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240414/요일 맞추기/guess-day-of-week.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int m1 = Integer.parseInt(st.nextToken());
+        int m2 = Integer.parseInt(st.nextToken());
+        int d1 = Integer.parseInt(st.nextToken());
+        int d2 = Integer.parseInt(st.nextToken());
+
+        String[] date = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
+
+        //1    2    3   4   5   6    7    8   9   10   11  12
+        int[] monthOfDay = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+        int diff = ((monthOfDay[m2] + d2) - (monthOfDay[m1] + d1)) + 2;
+
+        if(diff < 0 ){
+            diff += 7;
+        }
+        System.out.println(date[diff%7]);
+
+    }
+}
