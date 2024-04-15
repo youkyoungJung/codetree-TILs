42b00105de905cdf6661c5b80b33415c151f7871
youkyoungJung <76727102+youkyoungJung@users.noreply.github.com>
2024-04-16 02:26:31 +0900

    Title: 최대 감소 부분 수열, Time: 120ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240416/최대 감소 부분 수열/longest-decreasing-subsequence.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n+1];
+        int[] dp = new int[n+1];
+
+        Arrays.fill(dp, 1);
+        dp[0] = 0;
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 1; i <= n; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for(int i = 1; i <= n; i++){
+            for(int j = 0; j < i; j++){
+                if(arr[j] > arr[i]){
+                    dp[i] = Math.max(dp[i], dp[j]+1);
+                }
+            }
+        }
+
+        int answer = -1;
+        for(int i = 1; i <= n; i++){
+            answer = Math.max(answer, dp[i]);
+        }
+        System.out.println(answer);
+    }
+}
