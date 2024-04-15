bf668fb58de7531de21d2f08d5bacd1620f4e252
youkyoungJung <76727102+youkyoungJung@users.noreply.github.com>
2024-04-16 02:33:15 +0900

    Title: 최대 점프 횟수, Time: 94ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240416/최대 점프 횟수/maximum-number-of-jumps.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[n+1];
+        int[] dp = new int[n+1];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 1; i <= n; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        } 
+
+        for(int i = 1; i <= n; i++){
+            for(int j = 0; j < i; j++){
+                if(j + arr[j] >= i){
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+
+        System.out.println(dp[n]);
+
+
+    }
+}
