34eb808b4c55b96f521a1a3c3cc013894523d971
youkyoungJung <76727102+youkyoungJung@users.noreply.github.com>
2024-04-16 10:40:50 +0900

    Title: 최장 공통 부분 수열, Time: 133ms, Memory: 13MB, Status: Passed - CodeTree

---
+++ 240416/최장 공통 부분 수열/longest-common-sequence.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		String first=br.readLine();
+		String second=br.readLine();
+
+		int n=first.length();
+		int m=second.length();
+
+		int[][] dp=new int[n+1][m+1];
+		for(int i=1;i<=n;i++){
+			for(int j=1;j<=m;j++){
+				if(first.charAt(i-1)==second.charAt(j-1)){
+					dp[i][j]=dp[i-1][j-1]+1;
+				}else{
+					dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+				}
+			}
+		}
+
+		// int max=0;
+		// for(int i=1;i<=n;i++){
+		// 	for(int j=1;j<=m;j++){
+		// 		max=Math.max(max,dp[i][j]);
+		// 	}
+		// }
+		System.out.println(dp[n][m]);
+
+	}
+}
