ceb1d7aeffe82386c5a3f3cac82419cb93b3e2a5
youkyoungJung <76727102+youkyoungJung@users.noreply.github.com>
2024-04-17 22:55:55 +0900

    Title: 숫자의 개수, Time: 932ms, Memory: 24MB, Status: Passed - CodeTree

--- 240417/숫자의 개수/number-of-integers.java
+++ 240417/숫자의 개수/number-of-integers.java
@@ -2,31 +2,64 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+    static int n;
+    static int m;
+    static int[] arr;
     public static void main(String[] args) throws IOException {
         // 여기에 코드를 작성해주세요.
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-
-        HashMap<Integer, Integer> map = new HashMap<>();
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new int[n];
         st = new StringTokenizer(br.readLine());
         for(int i = 0; i < n; i++){
-            int key = Integer.parseInt(st.nextToken());
-            map.put(key, map.getOrDefault(key, 0) + 1);
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
         for(int i = 0; i < m; i++){
             int target = Integer.parseInt(br.readLine());
-            if(map.containsKey(target)){
-                sb.append(map.get(target));
-            }else{
-                sb.append(0);
-            }
-            sb.append("\n");
+            int cnt = upperBound(target) - lowerBound(target);
+            System.out.println(cnt);
         }
 
-        System.out.println(sb);
     }
+
+    public static int upperBound(int target){
+        int left = 0;
+        int right = n-1;
+        int minIdx = n;
+
+        while(left <= right){
+            int mid = (left + right) / 2;
+
+            if(arr[mid] > target){
+                right = mid - 1;
+                minIdx = Math.min(minIdx, mid);
+            }else{
+                left = mid + 1;
+            }
+        }
+        return minIdx;
+    }
+
+    public static int lowerBound(int target){
+        int left = 0;
+        int right = n-1;
+        int minIdx = n;
+
+        while(left <= right){
+            int mid = (left + right) / 2;
+
+            if(arr[mid] >= target){
+                right = mid - 1;
+                minIdx = Math.min(minIdx, mid);
+            }else{
+                left = mid + 1;
+            }
+        }
+        return minIdx;
+    }
+
 }
