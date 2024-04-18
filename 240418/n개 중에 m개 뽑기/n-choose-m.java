96d29c13a21a499ea46961154a61fe893b15e4a1
youkyoungJung <76727102+youkyoungJung@users.noreply.github.com>
2024-04-18 18:41:15 +0900

    Title: n개 중에 m개 뽑기, Time: 107ms, Memory: 9MB, Status: Passed - CodeTree

--- 240418/n개 중에 m개 뽑기/n-choose-m.java
+++ 240418/n개 중에 m개 뽑기/n-choose-m.java
@@ -5,7 +5,6 @@ public class Main {
 
     static ArrayList<Integer> nums = new ArrayList<>();
     static StringBuilder sb = new StringBuilder();
-    static boolean[] isVisited;
     static int n, m;
 
     public static void main(String[] args) throws IOException {
@@ -15,30 +14,27 @@ public class Main {
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
 
-        isVisited = new boolean[n+1];
         go(0, 1);
 
         System.out.println(sb.toString());
 
     }
 
-    public static void go(int cnt, int start){
-        if(cnt == m){
-            for(int i : nums){
-                sb.append(i).append(" ");
+    public static void go(int cnt, int current){
+        if(current == n+1){
+            if(cnt == m){
+                for(int i : nums){
+                    sb.append(i).append(" ");
+                }
+                sb.append("\n");
             }
-            sb.append("\n");
             return;
         }
 
-        for(int i = start; i <= n; i++){
-            if(!isVisited[i])
-            isVisited[i] = true;
-            nums.add(i);
-            go(cnt+1, i+1);
-            isVisited[i] = false;
-            nums.remove(nums.size()-1);
-        }
+        nums.add(current);
+        go(cnt+1, current+1);
+        nums.remove(nums.size()-1);
 
+        go(cnt, current + 1);
     }
 }
