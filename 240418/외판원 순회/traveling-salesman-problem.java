dc5cd740f3769d71528d22e690261228df1de4bd
youkyoungJung <76727102+youkyoungJung@users.noreply.github.com>
2024-04-18 20:01:23 +0900

    Title: 외판원 순회, Time: 95ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240418/외판원 순회/traveling-salesman-problem.java
@@ -0,0 +1,55 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static ArrayList<Integer> nums = new ArrayList<>();
+    static int n;
+    static int[][] arr;
+    static boolean[] isVisited;
+
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n+1][n+1];
+        isVisited = new boolean[n+1];
+
+        for(int i = 1; i <= n; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for(int j = 1; j <= n; j++){
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        per(0);
+        System.out.println(answer);
+    }
+
+    static int answer = Integer.MAX_VALUE;
+    public static void per(int cnt){
+        if(cnt == n-1){
+            //총 최소 비용 확인
+            int res = 0;
+            int temp = 1;
+            // System.out.println(nums);
+            for(int i = 0; i < nums.size(); i++){ //행
+                res += arr[temp][nums.get(i)];
+                temp = nums.get(i);
+            }
+            res += arr[temp][1];
+            // System.out.println(res);
+            answer = Math.min(answer, res);
+            return;
+        }
+
+        for(int i = 2; i <= n; i++){
+            if(!isVisited[i]){
+                isVisited[i] = true;
+                nums.add(i);
+                per(cnt+1);
+
+                isVisited[i] = false;
+                nums.remove(nums.size()-1);
+            }
+        }
+    }
+}
