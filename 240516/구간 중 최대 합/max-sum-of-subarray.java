4c582b20732e2207370ffcba671a5bd4c04750e0
youkyoungJung <76727102+youkyoungJung@users.noreply.github.com>
2024-05-16 08:36:20 +0900

    Title: 구간 중 최대 합, Time: 109ms, Memory: 8MB, Status: Passed - CodeTree

--- 240516/구간 중 최대 합/max-sum-of-subarray.java
+++ 240516/구간 중 최대 합/max-sum-of-subarray.java
@@ -18,7 +18,7 @@ public class Main {
 
         int answer = 0;
 
-        for(int i = 0; i < n - k; i++){
+        for(int i = 0; i <= n - k; i++){
             int sum = 0;
             for(int j = i; j < i+k; j++){
                 sum += arr[j];
