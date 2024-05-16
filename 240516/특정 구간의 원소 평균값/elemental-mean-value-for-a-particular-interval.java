3759659ddd27adf42b72bf77e386724219f8de59
youkyoungJung <76727102+youkyoungJung@users.noreply.github.com>
2024-05-16 09:31:52 +0900

    Title: 특정 구간의 원소 평균값, Time: 182ms, Memory: 18MB, Status: Wrong Answer - CodeTree

--- 240516/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.java
+++ 240516/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.java
@@ -17,18 +17,23 @@ public class Main {
 
         int answer = 0;
         for(int i = 0; i < n; i++){
-            for(int j = i+1; j < n; j++){
+            for(int j = i; j < n; j++){
 
-                int sum = 0;
-                HashSet<Integer> set = new HashSet<>();
-                for(int k = i; k < j; k++){
-                    sum += arr[k];
-                    set.add(arr[k]);
-                }
-                int avg = sum / (j-i);
-
-                if(set.contains(avg)){
+                if(i == j){
                     answer++;
+                }else{
+                    int sum = 0;
+                    HashSet<Double> set = new HashSet<>();
+                    for(int k = i; k <= j; k++){
+                        sum += arr[k];
+                        set.add((double)arr[k]);
+                    }
+                    double avg = (double)sum / (j-i);
+                    if(set.contains(avg)){
+                        // System.out.println("sum : " + sum + " avg: " + avg  + " i : " + i + " j : " +  j);
+                        // System.out.println("avg: " + avg + " set: " + set);
+                        answer++;
+                    }
                 }
             }
         }
