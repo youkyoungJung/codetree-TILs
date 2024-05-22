82713aed30447021d25efa95d724e56b98394966
youkyoungJung <76727102+youkyoungJung@users.noreply.github.com>
2024-05-22 10:16:43 +0900

    Title: 밭의 높이를 고르게하기, Time: 87ms, Memory: 8MB, Status: Passed - CodeTree

--- 240522/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.java
+++ 240522/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.java
@@ -19,7 +19,7 @@ public class Main {
         }
 
         int answer = Integer.MAX_VALUE;
-        for(int i = 0; i < n - t; i++){
+        for(int i = 0; i <= n - t; i++){
             int cost = 0;
             for(int j = i; j < i + t; j++){
                 cost += Math.abs(arr[j] - h);
