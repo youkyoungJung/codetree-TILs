382179eac81003c9a158f5f7ccd4e5acdedfdbda
youkyoungJung <76727102+youkyoungJung@users.noreply.github.com>
2024-05-22 09:16:49 +0900

    Title: 바구니 안의 사탕 2, Time: 80ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240522/바구니 안의 사탕 2/candy-in-the-basket-2.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[] bucket = new int[300];
+
+        for(int i = 0; i < n; i++){
+            st = new StringTokenizer(br.readLine());
+            int candy = Integer.parseInt(st.nextToken());
+            int num = Integer.parseInt(st.nextToken()) + 100;
+
+            bucket[num] = candy;
+        }
+
+        int answer = 0;
+        for(int c = 100; c <= 200; c++){
+            int sum = 0;
+            for(int i = c - k; i <= c + k; i++){
+                sum += bucket[i];
+            }
+            answer = Math.max(answer, sum);
+        }
+
+        System.out.println(answer);
+    }
+}
