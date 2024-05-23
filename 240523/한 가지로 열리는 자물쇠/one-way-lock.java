28bf9ebeaa6193b19bd0a8ba6a30bea410ce714c
youkyoungJung <76727102+youkyoungJung@users.noreply.github.com>
2024-05-23 09:15:52 +0900

    Title: 한 가지로 열리는 자물쇠, Time: 140ms, Memory: 9MB, Status: Passed - CodeTree

--- 240523/한 가지로 열리는 자물쇠/one-way-lock.java
+++ 240523/한 가지로 열리는 자물쇠/one-way-lock.java
@@ -1,23 +1,29 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         // 여기에 코드를 작성해주세요.
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int n = Integer.parseInt(br.readLine());
+        // int total = n*n*n;
+
         StringTokenizer st = new StringTokenizer(br.readLine());
         //서로 다른 조합
         int[] arr = new int[3];
-        int a = 1;
         for(int i = 0; i < 3; i++){
             arr[i] = Integer.parseInt(st.nextToken());
-            a *= arr[i];
         }
 
-        int total = 6*6*6;
-        System.out.println(total - a);
+        int cnt = 0;
+        for(int i = 1; i <= n; i++)
+            for(int j = 1; j <= n; j++)
+                for(int k = 1; k <= n; k++)
+                    if(Math.abs(arr[0]- i) <= 2 || Math.abs(arr[1]- j) <= 2 || Math.abs(arr[2]- k) <= 2)
+                        cnt++;
+
+        System.out.println(cnt);
+        // System.out.println(total - cnt);
         
     }
 }
