2baf3da573889f68a8868f6f0d9bcd5414cbf84e
youkyoungJung <76727102+youkyoungJung@users.noreply.github.com>
2024-06-14 21:44:39 +0900

    Title: 고대 문명 유적 탐사, Time: 99ms, Memory: 9MB, Status: Wrong Answer - CodeTree

--- 240614/고대 문명 유적 탐사/ancient-ruin-exploration.java
+++ 240614/고대 문명 유적 탐사/ancient-ruin-exploration.java
@@ -2,47 +2,25 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int[][] arr;
-    // static int[][] temp;
-    static int k;
-    static int m;
-    static int[][] centers = {{1, 1}, {1, 2}, {1, 3},
-                              {2, 1}, {2, 2}, {2, 3},
-                              {3, 1}, {3, 2}, {3, 3}  }; //중심점
-
-    static class Location implements Comparable<Location> {
+    static class Location {
         int r;
         int c;
-        int num;
 
-        public Location(int r, int c, int num){
+        public Location(int r, int c){
             this.r = r;
             this.c = c;
-            this.num = num;
-        }
-
-        public int compareTo(Location o){
-            if(o.r == this.r){
-                return Integer.compare(o.c, this.c);
-            }
-            return Integer.compare(this.r, o.r);
         }
     }
-    static Location location;
-    static Queue<Integer> newOne = new ArrayDeque<>();
-    static int value;
-
     public static void main(String[] args) throws IOException {
         // 여기에 코드를 작성해주세요.
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         StringBuilder sb = new StringBuilder();
+        int k = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
-        k = Integer.parseInt(st.nextToken()); // 탐사의 반복 횟수
-        m = Integer.parseInt(st.nextToken()); // 유물 조각 개수
-
-        arr = new int[5][5];
-        // newOne = new int[m];
+        Queue<Integer> newOne = new ArrayDeque<>();
+        int[][] arr = new int[5][5];
 
         for(int i = 0; i < 5; i++){
             st = new StringTokenizer(br.readLine());
@@ -54,132 +32,128 @@ public class Main {
         st = new StringTokenizer(br.readLine());
         for(int i = 0; i < m; i++){
             newOne.offer(Integer.parseInt(st.nextToken()));
-            // newOne[i] = Integer.parseInt(st.nextToken());
         }
 
-        while(k-- > 0){//탐사 반복
+        while(k-- > 0){
+            int maxScore = 0;
+            int[][] maxArr = new int[5][5];
 
-            value = 0;
-            simulation();
-            sb.append(value).append(" ");
-
-        }
-
-        System.out.println(sb.toString());
-    
-    }
-
-    public static void search(){ // 가장많이 터지는 위치 탐지
-        location = new Location(-1, -1, -1);
-
-        Queue<Location> queue = new ArrayDeque<>();
-        for(int i = 0; i < 9; i++){
-            int r = dist[i][0];
-            int c = dist[i][1];
-            queue.offer(new Location(r, c, arr[r][c]));
-        }
-
-        while(!queue.isEmpty()){
-            Location current = queue.poll();
-
-            for(int i = 0; i < 3; i++){
-                goRot(current.r, current.c); //배열 돌리기
-                goCon();//조각 3개 이상 연결된 경우 찾기
+            for(int i = 0; i < 3; i++){ //90, 180, 270
+                for(int r = 0; r <= 2; r++){
+                    for(int c = 0; c <= 2; c++){
+                        int[][] rotated = rotate(r, c, i, arr);
+                        int score = calcScore(rotated);
+                        if(maxScore < score){
+                            maxScore = score;
+                            maxArr = rotated;
+                        }
+                    }
+                }
             }
-        }
 
+            if(maxArr[0][0] == 0){ // 더 이상 획득 못한경우
+                break;
+            }
+
+            arr = maxArr;
+            while(true){
+                arr = fill(newOne, arr);
+                int newScore = calcScore(arr);
+                if(newScore == 0) break;
+                maxScore += newScore;
+            }
+
+            sb.append(maxScore).append(" ");
+
+        }
+        System.out.println(sb.toString());
     }
 
-    public static void goRot(int r, int c){ // 90도 회전 하는 함수
-        
-        int[][] temp = new int[5][5];
-
-        for(int i = 0; i < 5; i++){
-            for(int j = 0; j < 5; j++){
-                if(i >= r-1 && i <= r+1 && j >=c-1 && j <= c+1){
-                    temp[i][j] = arr[2-j][i];
+    public static int[][] fill(Queue<Integer> queue, int[][] arr){
+        for(int j = 0; j < 5; j++){
+            for(int i = 4; i >= 0; i--){
+                if(arr[i][j] == 0 && !queue.isEmpty()){
+                    arr[i][j] = queue.poll();
                 }
             }
         }
-
-        temp = arr;        
-        
+        return arr;
     }
 
     static boolean[][] isVisited;
+    static int[][] dist = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 
-    public static void goCon(){ //bfs
+    public static int calcScore(int[][] arr){
+        int score = 0;
         isVisited = new boolean[5][5];
         for(int i = 0; i < 5; i++){
             for(int j = 0; j < 5; j++){
                 if(!isVisited[i][j]){
-                    bfs(i, j);
-                }
-            }
-        }
-    }
+                    //bfs
+                    Queue<Location> queue = new ArrayDeque<>();
+                    Queue<Location> trace = new ArrayDeque<>();
+                    queue.offer(new Location(i, j));
+                    queue.offer(new Location(i, j));
+                    isVisited[i][j] = true;
 
-    static int[][] dist = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-    static ArrayList<Location> list = new ArrayList<>();
+                    while(!queue.isEmpty()){
+                        Location current = queue.poll();
+                        int target = arr[current.r][current.c];
+                        for(int d = 0; d < 4; d++){
+                            int nr = current.r + dist[d][0];
+                            int nc = current.c + dist[d][1];
 
-    public static void bfs(int r, int c){
-        
-        Queue<Location> queue = new ArrayDeque<>();
-        ArrayList<Location> temp = new ArrayList<>();
-        queue.offer(new Location(r, c, arr[r][c]));
-        isVisited[r][c] = true;
+                            if(isChecked(nr, nc) && arr[nr][nc] == target){
+                                queue.offer(new Location(nr, nc));
+                                trace.offer(new Location(nr, nc));
+                                isVisited[nr][nc] = true;
+                            }
+                        }
+                    }
+                    
+                    if(trace.size()>=3){
+                        score += trace.size();
 
-        int number = arr[r][c];
-        int cnt = 0;
+                        while(!trace.isEmpty()){
+                            Location current = trace.poll();
+                            arr[current.r][current.c] = 0;
+                        }
 
-        while(!queue.isEmpty()){
-            Location current = queue.poll();
-
-            for(int i = 0; i < 4; i++){
-                int nr = current.r + dist[i][0];
-                int nc = current.c + dist[i][1];
-
-                if(checked(nr, nc)){
-                    if(number == arr[nr][nc]){
-                        cnt++;
-                        isVisited[nr][nc] = true;
-                        temp.add(new Location(nr, nc, arr[nr][nc]));
-                        queue.offer(new Location(nr, nc, 0));
                     }
                 }
             }
         }
+        return score;
+    } // end of clac
 
-        if(cnt >= 3){
-            if(value < cnt){
-                list = temp;
-                value = cnt;
-                location.r = r;
-                location.c = c;
-                location.num = temp[r][c];
+    public static boolean isChecked(int r, int c){
+        return r >= 0 && r < 5 && c >= 0 && c < 5 && !isVisited[r][c];
+    }
+
+    //rotate(r, c, i, arr);
+    public static int[][] rotate(int r, int c, int l, int[][] arr){
+        
+        int[][] result = new int[5][5];
+        int n = arr.length;
+        for(int i = 0; i < 5; i++){
+            for(int j = 0; j < 5; j++){
+                result[i][j] = arr[i][j];
             }
         }
-    }
 
-    public static boolean checked(int nr, int nc){
-        return nr >= 0 && nr < 5 && nc >= 0 && nc < 5 && !isVisited[nr][nc];
-    }
-
-    public static void simulation(){
-        search();
-        remove(); // 폭발하면서 value 값 올리기 
-    }
-
-    public static void remove(){
-       
-        for(int i = 0; i < list.size(); i++){
-            Location current = list.get(i);
-            int r = current.r;
-            int c = current.c;
-            value += arr[r][c];
-            arr[r][c] = 0;
-            arr[r][c] = newOne.poll();
+        for(int i = r; i < r+3; i++){
+            for(int j = c; j < c+3; j++){
+                if(l == 0){ //90도
+                    result[i][j] = arr[n-1-j][i];
+                }else if(l == 1){ //180도
+                    result[i][j] = arr[n-1-i][n-1-j];
+                }else{ //270도
+                    result[i][j] = arr[j][n-1-i];
+                }
+            }
         }
 
+        return result;
     }
-}
+
+}//end of class
